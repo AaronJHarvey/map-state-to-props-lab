343bb5123639094a3e942f2e5f4b926749c53633
AaronJHarvey <Harvey.AaronJ@gmail.com>
2021-11-20 01:09:41 -0500

    fin

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-// add any needed imports here
-class Users extends Component {
+import {connect} from 'react-redux'
 
+class Users extends Component {
   render() {
+    let users = this.props.users.map((user,index) => <li key={index}>{user.username}</li>)
+      //creates a variable called users that has a value of the users list found in props for the Users component
+      // for each item in the users list it maps usign creates a list entry. where the key is the index of the item and the user's username is displayed.
     return (
       <div>
+        {this.props.numberOfUsers}
         <ul>
-          Users!
-          {/* Write code here that displays the usernames of all users in the Redux store */}
-          {/* In addition, display the total number of users curently in the store */}
+          {users}
         </ul>
       </div>
     )
   }
 }
 
-//add mapStateToProps here
 
+// mapStateToProps
+// Takes in the state that is passed down to it using Provider as an argument.
+// returns a JavaScript Object with two keys - users and numberOfUsers
+// each of these keys have values that they get via the state
+const mapStateToProps = (state) =>{
+  return {
+    users: state.users,
+    numberOfUsers: state.users.length
+  }
+}
 // connect this component to Redux
-export default Users
+//connects this component (Users) to the redux library allowing it to get pieces of the data from the state
+// without this we would not be able to set the above JS object's values from the state
+export default connect(mapStateToProps)(Users)
